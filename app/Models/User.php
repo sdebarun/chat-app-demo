bb32cbc5413d6dbc1d9f1175ed83cb7e44b6747e
sdebarun <sdebaurn04@gmail.com>
2024-04-15 17:53:06 +0530

    backpack and spatie added

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Message;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
